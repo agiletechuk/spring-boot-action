416b2c47ae790c43a3434fb86e585cc2b4f4871e
steve <steve@AgileTechs-M3.local>
2025-04-11 12:33:10 +0100

    add github action

---
+++ src/main/java/com/example/gitub/action/SpringbootGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitub.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitub/action/SpringbootGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitub.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
